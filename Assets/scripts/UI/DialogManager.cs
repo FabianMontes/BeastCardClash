bf099b3217a5915a3523dca7119a30c31520334d
Hayran Andrés López <andreslopez01182009@gmail.com>
2025-08-10 13:23:18 -0500

    Implementación de diálogos (parte 1)
    Con el archivo `Dialogs.json`, el script `DialogManager.cs` se mueve por la secuencia de diálogos que esta establecido, en función de los 4 estados del juego:
    
    1. Al inicio
    2. Antes de la batalla
    3. Si gana
    4. Si pierde
    
    El siguiente paso es usar un segundo script para gestionar la secuencia del juego, el movimiento, el inicio de la batalla, etc.

--- Assets/scripts/UI/DialogManager.cs
+++ Assets/scripts/UI/DialogManager.cs
@@ -1,21 +1,61 @@
 using TMPro;
 using UnityEngine;
 
+// Estados del juego (normal, antes de jugar, al ganar y al perder)
+public enum GameStates
+{
+    normal, preGame, win, lose
+}
+
+// Extrae los diálogos desde el JSON
+[System.Serializable]
+public class DialogFile
+{
+    public Dialogs[] Dialogs;
+    public Dialogs[] PreGameDialogs;
+    public Dialogs[] WinDialogs;
+    public Dialogs[] LoseDialogs;
+}
+
+// Extrae los diálogos desde DialogFile
+[System.Serializable]
+public class Dialogs
+{
+    public string character;
+    public string text;
+}
+
 public class DialogManager : MonoBehaviour
 {
+    // Componentes de UI
     [SerializeField] private GameObject dialogPanel; // Panel de diálogo en la UI
     [SerializeField] private TextMeshProUGUI namePanel; // Panel de nombre del personaje en la UI
-    [SerializeField] private string characterName; // Nombre del personaje
-    [SerializeField] private float maxDistance = 3f; // Distancia máxima para mostrar el diálogo
-    [SerializeField] private Transform target; // Target (jugador)
+    [SerializeField] private TextMeshProUGUI textPanel; // Panel de texto del diálogo en la UI
 
-    private Target targetMovement; // Script de movimiento del target (necesario para desactivarlo)
+    [SerializeField] private string characterName; // Nombre del personaje
+
+    // Componentes de jugador y juego
+    [SerializeField] private Transform target; // Target (jugador)
+    [SerializeField] private float maxDistance = 3f; // Distancia máxima para mostrar el diálogo
+    [SerializeField] private GameStates gameState; // Estado actual del juego
+    private Target targetScript; // Script de movimiento del target (necesario para desactivarlo)
+
+    // Componentes de diálogos
+    [SerializeField] private TextAsset dialogFile; // Archivo JSON con los diálogos
+    private DialogFile dialogFileContent; // Datos extraídos del archivo JSON
+    private Dialogs currentDialog; // Diálogo actual
+    private int currentDialogIndex; // Índice del diálogo actual
+    private bool inDialog; // Indica si el jugador está en un diálogo
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        targetMovement = target.GetComponent<Target>();
-        dialogPanel?.SetActive(false); // Oculta el panel al inicio. El operador de acceso condicional a nulos (?.) simplifica la comprobación
+        // Carga el script del target y oculta el panel de diálogo
+        targetScript = target.GetComponent<Target>();
+        dialogPanel?.SetActive(false); // El operador de acceso condicional a nulos (?.) simplifica la comprobación
+
+        // Cargar el archivo JSON en el objeto
+        if (dialogFile != null) dialogFileContent = JsonUtility.FromJson<DialogFile>(dialogFile.text);
     }
 
     // Update is called once per frame
@@ -25,12 +65,77 @@ public class DialogManager : MonoBehaviour
         float distance = Vector3.Distance(transform.position, target.position);
         bool targetInRange = distance <= maxDistance;
 
-        // Si el jugador está en rango y presiona 'Z', alterna el panel y desactiva el movimiento
-        if (targetInRange && Input.GetKeyDown(KeyCode.Z))
+        // Si el jugador está en rango y presiona 'Z', llama a la función de diálogo
+        // De lo contrario, verifica que esté en diálogo y si se presiona 'Z', avanza al siguiente diálogo
+        if (!inDialog && targetInRange && Input.GetKeyDown(KeyCode.Z))
         {
-            dialogPanel.SetActive(!dialogPanel.activeSelf); // Alterna la visibilidad del panel
-            targetMovement.enabled = !dialogPanel.activeSelf; // Alterna el movimiento de forma opuesta al panel
-            if (dialogPanel.activeSelf) namePanel.text = characterName; // Muestra el nombre en el panel, si está activo
+            ShowDialogPanel();
+        }
+        else if (inDialog && Input.GetKeyDown(KeyCode.Z))
+        {
+            currentDialogIndex++;
+            DisplayDialog();
+        }
+    }
+
+    // Cambia el estado del juego, desde afuera
+    public void SetGameState(GameStates newState)
+    {
+        gameState = newState;
+    }
+
+    // Muestra el panel de diálogo
+    void ShowDialogPanel()
+    {
+        if (inDialog) return; // Evita reiniciar si ya estás en diálogo
+
+        // Activa los diálogos y desactiva el movimiento
+        inDialog = true;
+        targetScript.enabled = false;
+        dialogPanel.SetActive(true);
+
+        currentDialogIndex = 0; // Pone el diálogo inicial (posición 0)
+        DisplayDialog();
+    }
+
+    // Extrae la secuencia de diálogos según el estado actual del juego
+    Dialogs[] GetDialogsForState()
+    {
+        switch (gameState)
+        {
+            case GameStates.normal:
+                return dialogFileContent.Dialogs;
+            case GameStates.preGame:
+                return dialogFileContent.PreGameDialogs;
+            case GameStates.win:
+                return dialogFileContent.WinDialogs;
+            case GameStates.lose:
+                return dialogFileContent.LoseDialogs;
+            default:
+                return new Dialogs[0]; // Si el estado actual no es reconocido, devuelve un array vacío
+        }
+    }
+
+    // Muestra el siguiente diálogo
+    void DisplayDialog()
+    {
+        var dialogs = GetDialogsForState(); // Obtiene los diálogos según el estado actual
+
+        // Verifica que estemos en medio del diálogo y que haya más diálogos
+        // Si es así, muestra el diálogo
+        // Si no, cierra el diálogo y activa el movimiento
+        if (currentDialogIndex < dialogs.Length)
+        {
+            namePanel.text = dialogs[currentDialogIndex].character;
+            textPanel.text = dialogs[currentDialogIndex].text;
+            currentDialog = dialogs[currentDialogIndex];
+        }
+        else
+        {
+            inDialog = false;
+            dialogPanel.SetActive(false);
+            targetScript.enabled = true;
+            currentDialogIndex = 0; // Reinicia el índice del diálogo
         }
     }
 }
